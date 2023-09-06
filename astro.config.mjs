c931928115cc4ee57654331567ea0c0c1c32644b
Gurkan <hello@gurkz.me>
2023-09-06 17:30:27 +0200

    add sitemap and prepare for custom deploy w/ node

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,15 @@
 import { defineConfig } from 'astro/config';
-
 import tailwind from "@astrojs/tailwind";
+
+import sitemap from "@astrojs/sitemap";
+import node from "@astrojs/node";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  site: "https://gurkz.me",
+  integrations: [tailwind(), sitemap()],
+  output: "server",
+  adapter: node({
+    mode: "standalone"
+  })
 });
