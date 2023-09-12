76e42a07e396fefc71ca16b2d48f2a4ebac32fe7
Gurkan <hello@gurkz.me>
2023-09-12 17:47:29 +0200

    change the home page to say what the website is about

--- astro.config.ts
+++ astro.config.ts
@@ -1,8 +1,7 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
-import vercel from "@astrojs/vercel/serverless";
-
+import vercel from '@astrojs/vercel/serverless';
 import solidJs from "@astrojs/solid-js";
 import { socials } from './src/config';
 
@@ -11,6 +10,8 @@ export default defineConfig({
   site: "https://gurkz.me",
   integrations: [tailwind(), sitemap(), solidJs()],
   output: "server",
-  adapter: vercel(),
+  adapter: vercel({
+    functionPerRoute: false
+  }),
   redirects: socials
 });
