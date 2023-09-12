8efcb6acf221f70a098d05a865ea37f1637f24d4
Gurkan <hello@gurkz.me>
2023-09-12 15:59:43 +0200

    change to vercel

--- astro.config.ts
+++ astro.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
-import node from "@astrojs/node";
+import vercel from "@astrojs/vercel/serverless";
 
 import solidJs from "@astrojs/solid-js";
 import { socials } from './src/config';
@@ -11,8 +11,6 @@ export default defineConfig({
   site: "https://gurkz.me",
   integrations: [tailwind(), sitemap(), solidJs()],
   output: "server",
-  adapter: node({
-    mode: "standalone"
-  }),
+  adapter: vercel(),
   redirects: socials
 });
